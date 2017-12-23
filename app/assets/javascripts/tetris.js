27f0cf1ba670b20decc12ca7e97e55968023b52b
mwmayerle <mwmayerle@gmail.com>
2017-12-23 14:49:20 -0600

    able to draw background as a series of individual blocks

--- app/assets/javascripts/tetris.js
+++ app/assets/javascripts/tetris.js
@@ -1,19 +1,42 @@
 /*
-
 The tetris NES window is 10 blocks wide and 20 blocks tall
 
 fillRect(x coordinate, y coordinate, width, height)
 
+Current plan:
+	Each shape will be an object composed of blocks made with a constructor function
+	Each shape has an arrangement of blocks (their coordinates) and a color as inputs
+	Draw 200 cubes and keep changing colors as time goes on
 */
 
 window.onload = function() {
 	drawBackground();
 };
 
+
+/* Background consists of 10 x 20 cubes colored black. Each row is generated within the
+	drawRow() function. drawRow() increments x coordinates while holding y coordinates steady
+*/
 var drawBackground = function() {
 	var tetrisMain = document.getElementById("tetris");
 	var tetrisMainContext = tetrisMain.getContext("2d");
 
-	tetrisMainContext.fillStyle = "black";
-	tetrisMainContext.fillRect(0,0,500,1000);
+	var rowNum;
+	var cubeY = 0;
+
+	for (rowNum = 0; rowNum < 20; rowNum++) {
+		drawRow(tetrisMain, tetrisMainContext, rowNum);
+	}
+}
+
+var drawRow = function(tetrisMain, tetrisMainContext, rowNum) {
+	rowNum *= 50; 
+	var cubeX = 0;
+	var currentBlock;
+
+	for (currentBlock = 0; currentBlock < 10; currentBlock++) {
+		tetrisMainContext.fillStyle = "black";
+		tetrisMainContext.fillRect(cubeX, rowNum, 50, 50);
+		cubeX += 50;
+	}
 }
