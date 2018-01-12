ccc971a27abdef4f763e1bfb8880dc8bf1e7e97f
mwmayerle <mwmayerle@gmail.com>
2018-01-11 20:33:24 -0600

    tenatively working with  a 4 layer deep nested if statement inside a loop I'm ashamed of

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -6,20 +6,37 @@ var Game = function() {
 	this.nextShape = '';
 };
 
-/*
-	It seems like if it wants to clear 3 rows it clears the next 3 rows on the 2 turns afterwards, not all at once. Why?
-*/
-
 Game.prototype.checkForCompleteRow = function() {
 	for (var rowYCoords = 950; rowYCoords >= 0; rowYCoords -= 50) {
 		var amtInRow = [];
-		amtInRow = this.occupiedPositions.filter(function(occupiedPosition){
-			return rowYCoords === occupiedPosition[0][1];
+
+		amtInRow1 = this.occupiedPositions.filter(function(occupiedPosition){
+			return rowYCoords === occupiedPosition[0][1] + 0;
 		});
-		if (amtInRow.length === 10) {
+		amtInRow2 = this.occupiedPositions.filter(function(occupiedPosition){
+			return rowYCoords === occupiedPosition[0][1] + 50;
+		});
+		amtInRow3 = this.occupiedPositions.filter(function(occupiedPosition){
+			return rowYCoords === occupiedPosition[0][1] + 100;
+		});
+		amtInRow4 = this.occupiedPositions.filter(function(occupiedPosition){
+			return rowYCoords === occupiedPosition[0][1] + 150;
+		});
+		if (amtInRow1.length === 10) {
 			this.deleteRow(rowYCoords);
+			if (amtInRow2.length === 10) {
+				this.deleteRow(rowYCoords);
+				if (amtInRow3.length === 10) {
+					this.deleteRow(rowYCoords);
+					if (amtInRow4.length === 10) {
+						this.deleteRow(rowYCoords);
+					}
+				}
+			}
 		}
+
 	}
+	this.redrawTetrominos();
 };
 
 Game.prototype.deleteRow = function(rowYCoord) {
@@ -35,7 +52,6 @@ Game.prototype.deleteRow = function(rowYCoord) {
 		rowIndicies.splice(toKillIndex, 1);
 	}
 	this.moveDownEverything(rowYCoord);
-	this.redrawTetrominos();
 };
 
 Game.prototype.getIndiciesToDelete = function(rowYCoord) {
